9d01ffb37475a9fd6c011e3f8a53d508657acaa6
Hassan @VirtualBox <hassansalehe@gmail.com>
2017-02-19 13:46:11 +0300

    Added a racy but a correct example

---
+++ src/banking_task_serial.cpp
@@ -0,0 +1,80 @@
+///////////////////////////////////////////////////////////////////////
+//
+//                  (c) 2017 - Hassan Salehe Matar
+//
+//
+//  Description:
+//
+//   *  This program creates 4 tasks namely INITIALIZER, COMMISSIONER,
+//      DEPOSITOR, and WITHDRAWER.
+//
+//   *  The expected execution order of these tasks is
+//      INITIALIZER -> COMMISSIONER -> DEPOSITOR -> WITHDRAWER.
+//      This execution order is ensured by the data flow dependency 
+//      specified using the depend clause of OpenMP task pragma.
+//  
+//   *  Unfortunately, Archer reports races on this setting.
+//
+//////////////////////////////////////////////////////////////////////
+#include <iostream>
+#include <cstring>
+
+#include <omp.h>
+
+using namespace std;
+
+// balance
+float balance;
+
+// tokens
+int token1;
+int token2;
+int token3;
+
+
+int main() {
+
+  #pragma omp parallel num_threads(4)
+  {
+    #pragma omp single
+    {
+      cout << "There are " << omp_get_num_threads() << " threads"<< endl;
+
+      // INITIALISER task
+      #pragma omp task depend(out:token1)
+      {
+        balance  = 1000; // set balance
+        token1 = 1; // trigger next task
+      }
+      
+  
+      // COMMISSIONER task
+      #pragma omp task depend(in:token1) depend(out:token2)
+      {
+        float rate = 0.2; // commission rate
+        balance += (balance * rate); // add commission
+        token2 = 1; // trigger next task
+      }
+        
+      // DEPOSITOR task
+      #pragma omp task depend(in:token2) depend(out:token3)
+      {
+        int amount = 200;
+        balance += amount;
+        token3 = 1; // trigger next task
+      }
+        
+      // WITHDRAWER task
+      #pragma omp task depend(in:token3)
+      {
+        int amount = 500; // amount to withdraw
+        balance -= amount; // deduce amount withdrawn
+      }
+        
+      // wait all tasks to complete
+      #pragma omp taskwait
+      cout << "balance: " << balance << endl;
+    }
+  }
+  return 0;
+}
